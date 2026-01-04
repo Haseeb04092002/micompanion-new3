0f4bc7734053db929b63eef07334e28ab70c42d7
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-01-04 18:46:26 +0500

    update

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    MiCompanion PWA – Service Worker
    ========================================================= */
 
-const CACHE_VERSION = "micompanion-v1.0.6";
+const CACHE_VERSION = "micompanion-v1.0.7";
 const CACHE_NAME = `micompanion-cache-${CACHE_VERSION}`;
 
 /* ---------------------------------------------------------

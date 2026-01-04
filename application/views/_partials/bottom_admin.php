3268066a9da52a11ea9afbc66fc56d1d00aefff8
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-01-04 17:21:03 +0500

    Update bottom_admin.php

--- application/views/_partials/bottom_admin.php
+++ application/views/_partials/bottom_admin.php
@@ -2,27 +2,27 @@
 
 <div class="bottom-bar d-flex">
 
-  <a href="<?= site_url('admin/admin_dashboard') ?>">
+  <a href="<?= site_url('admin/admin_dashboard') ?>" class="text-light">
     <i class="bi bi-speedometer2"></i>
     Dashboard
   </a>
 
-  <a href="<?= site_url('admin/admin_customers') ?>">
+  <a href="<?= site_url('admin/admin_customers') ?>" class="text-light">
     <i class="bi bi-people"></i>
     Customers
   </a>
 
-  <a href="<?= site_url('admin/admin_drivers') ?>">
+  <a href="<?= site_url('admin/admin_drivers') ?>" class="text-light">
     <i class="bi bi-truck"></i>
     Drivers
   </a>
 
-  <a href="<?= site_url('admin/admin_cargo') ?>">
+  <a href="<?= site_url('admin/admin_cargo') ?>" class="text-light">
     <i class="bi bi-box-seam"></i>
     Cargo
   </a>
 
-  <a href="<?= site_url('admin/admin_notifications') ?>">
+  <a href="<?= site_url('admin/admin_notifications') ?>" class="text-light">
     <i class="bi bi-bell"></i>
     Alerts
   </a>

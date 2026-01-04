d970846eac48b8a316535dcd0e19aeaa2dc34376
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-01-04 17:16:33 +0500

    update

--- application/views/_partials/bottom_admin.php
+++ application/views/_partials/bottom_admin.php
@@ -2,27 +2,27 @@
 
 <div class="bottom-bar d-flex">
 
-  <a href="<?= site_url('admin/admin_dashboard') ?>" class="btn btn-sm rounded-circle btn-outline-primary">
+  <a href="<?= site_url('admin/admin_dashboard') ?>" class="btn btn-sm btn-outline-primary">
     <i class="bi bi-speedometer2"></i>
     Dashboard
   </a>
 
-  <a href="<?= site_url('admin/admin_customers') ?>" class="btn btn-sm rounded-circle btn-outline-primary">
+  <a href="<?= site_url('admin/admin_customers') ?>" class="btn btn-sm btn-outline-primary">
     <i class="bi bi-people"></i>
     Customers
   </a>
 
-  <a href="<?= site_url('admin/admin_drivers') ?>" class="btn btn-sm rounded-circle btn-outline-primary">
+  <a href="<?= site_url('admin/admin_drivers') ?>" class="btn btn-sm btn-outline-primary">
     <i class="bi bi-truck"></i>
     Drivers
   </a>
 
-  <a href="<?= site_url('admin/admin_cargo') ?>" class="btn btn-sm rounded-circle btn-outline-primary">
+  <a href="<?= site_url('admin/admin_cargo') ?>" class="btn btn-sm btn-outline-primary">
     <i class="bi bi-box-seam"></i>
     Cargo
   </a>
 
-  <a href="<?= site_url('admin/admin_notifications') ?>" class="btn btn-sm rounded-circle btn-outline-primary">
+  <a href="<?= site_url('admin/admin_notifications') ?>" class="btn btn-sm btn-outline-primary">
     <i class="bi bi-bell"></i>
     Alerts
   </a>

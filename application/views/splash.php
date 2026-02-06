6c343766eeee6830cd9df15769cb7771738f6472
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-02-06 21:29:25 +0500

    up

--- application/views/splash.php
+++ application/views/splash.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>MiCompanion</title>
+    <title>Aramex Cargo : Aramex – Cargo Delivery & Logistics Management</title>
 
     <style>
         :root {
